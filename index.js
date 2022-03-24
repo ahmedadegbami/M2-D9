7b5108b8ee9698a5c0b41fb0fb23594bb68042b7
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-03-24 16:40:58 +0100

    updated

---
+++ index.js
@@ -0,0 +1,20 @@
+function deleteRow1() {
+    let trNodes1=document.querySelector(".trclass1")
+    
+        trNodes1.remove()
+    }
+
+function deleteRow2() {
+    let trNodes2=document.querySelector(".trclass2")
+    
+        trNodes2.remove()
+    }
+
+function deleteRow3() {
+    let trNodes3=document.querySelector(".trclass2")
+    
+    trNodes3.remove()
+    }
+
+
+
