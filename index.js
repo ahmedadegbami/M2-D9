60acb1aef4ceb7eb355da8ad86be38523820b099
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-03-24 19:16:48 +0100

    Updated

--- index.js
+++ index.js
@@ -17,4 +17,51 @@ function deleteRow3() {
     }
 
 
+// function addAlbum() {
+//     let table = document.getElementById("myTable")
+//     let row = document.createElement("tr")
+//     for (let i=0; i <= 3; i++) {
+//         let column = document.createElement("td")
+//         row.appendChild(column)
 
+//     }
+//     table.appendChild(row)
+
+
+function addAlbum() {
+    let table = document.getElementById("myTable")
+    let newRow = table.insertRow(table.rows.length)
+
+   
+
+
+
+
+    let track = newRow.insertCell(0);
+    let title = newRow.insertCell(1);
+    let duration = newRow.insertCell(2);
+    
+    
+
+    
+    let trackid = document.getElementById("trackid")
+    let titleid = document.getElementById("titleid")
+    let durationid = document.getElementById("durationid")
+
+   
+    
+    
+    
+    title.innerHTML = titleid.value;
+    track.innerHTML = trackid.value;
+    duration.innerHTML = durationid.value;
+    
+
+    let td4= document.getElementsByTagName("tr")[4]
+    let btn= document.createElement("button")
+    btn.innerHTML = "Delete"
+    
+    td4.appendChild(btn)
+    
+
+}
